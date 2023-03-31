c701520306b12ee559fedbab78bfca77a2995ae5
calvct <112923033+calvct@users.noreply.github.com>
2023-03-31 19:41:49 +0700

    Add files via upload

---
+++ THA_W5_Calvin_T/THA_W5_Calvin_T/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THA_W5_Calvin_T
+{
+    internal class Category
+    {
+        public string categoryName { get; set; }
+        public string categoryID { get; set; }
+    }
+}
